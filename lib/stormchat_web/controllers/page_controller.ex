bc4254af0d7654430513d647e7d70d82687f22f9
gautam <baghel.g@husky.neu.edu>
2018-04-18 00:21:22 -0400

    some more view changes

--- lib/stormchat_web/controllers/page_controller.ex
+++ lib/stormchat_web/controllers/page_controller.ex
@@ -13,30 +13,37 @@ defmodule StormchatWeb.PageController do
     logged_in = Session.logged_in?(conn)
 
     case logged_in do
-       false ->
-         changeset = User.changeset(%User{})
-         locations = Locations.list_locations
-         render conn, "index_not_logged.html", changeset: changeset, locations: locations
+      false ->
+        changeset = User.changeset(%User{})
+        locations = Locations.list_locations
+        render conn, "index_not_logged.html", changeset: changeset, locations: locations
 
-       true ->
-         user_id = conn |> get_session(:current_user)
-         user = Accounts.get_by_id!(user_id)
-         location = Locations.get_by_abbr!(user.location).name
+        true ->
+          user_id = conn |> get_session(:current_user)
+          user = Accounts.get_by_id!(user_id)
+          location = Locations.get_by_abbr!(user.location).name
 
-         {:ok, pid} = CallAPI.start_link([])
-         data = CallAPI.get_weather(pid, user.location)
+          {:ok, pid} = CallAPI.start_link([])
+          data = CallAPI.get_weather(pid, user.location)
 
-         dataMap = Enum.reduce data, %{}, fn x, acc ->
+          dataMap = Enum.reduce data, %{}, fn x, acc ->
             Map.put(acc, x["properties"]["id"], x["properties"])
-         end
+          end
 
-         render conn, "index.html", user: user, location: location, alerts: dataMap
-    end
-  end
+          render conn, "index.html", user: user, location: location, alerts: dataMap
+        end
+      end
 
-  def chat(conn, %{"id" => id, "headline" => headline}) do
+      def chat(conn, %{"id" => id, "severity" => severity,
+      "category" => category, "certainty" => certainty,
+      "response" => response, "instruction" => instruction,
+      "event" => event, "areaDesc" => areaDesc}) do
+
         user_id = conn |> get_session(:current_user)
-        render conn, "chat.html", alert_id: id, headline: headline, user_id: user_id
-  end
+        render conn, "chat.html", alert_id: id, severity: severity,
+        user_id: user_id, category: category, certainty: certainty,
+        response: response, instruction: instruction, event: event,
+        areaDesc: areaDesc
+      end
 
-end
+    end
