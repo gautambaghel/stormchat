8ca249647a46d202b7e9854cacc982648bbf1aaf
gautam <baghel.g@husky.neu.edu>
2018-09-03 02:19:31 -0400

    User creation via API done

--- lib/stormchat_web/controllers/token_controller.ex
+++ lib/stormchat_web/controllers/token_controller.ex
@@ -6,6 +6,32 @@ defmodule StormchatWeb.TokenController do
 
   action_fallback StormchatWeb.FallbackController
 
+  def new(conn, %{"name"=> name, "email" => email, "pass" => pass,
+                  "location" => location, "subscribe" => subscribe}) do
+      user_params = %{"name"=> name, "email" => email, "password" => pass,
+                      "location" => location, "subscribed" => subscribe}
+
+      case Accounts.create_user(user_params) do
+        {:ok, user} ->
+           token = Phoenix.Token.sign(conn, "auth token", user.id)
+           conn
+             |> put_status(:created)
+             |> render("token.json", user: user, token: token)
+        {:error, changeset} ->
+           err =
+           try do
+             [email: {msg, []}] = changeset.errors
+             err = "Email " <> msg <> " !"
+           catch
+             _ ->
+             err = "Error in creating user please verify the info entered"
+           end
+           conn
+             |> put_status(:unprocessable_entity)
+             |> render("error.json", error: err)
+      end
+  end
+
   def create(conn, %{"email" => email, "pass" => pass}) do
     with {:ok, %User{} = user} <- Accounts.get_and_auth_user(email, pass) do
       token = Phoenix.Token.sign(conn, "auth token", user.id)
@@ -25,5 +51,6 @@ defmodule StormchatWeb.TokenController do
       conn
         |> render("delete_token.json")
   end
+
 
 end
--- lib/stormchat_web/router.ex
+++ lib/stormchat_web/router.ex
@@ -33,6 +33,8 @@ defmodule StormchatWeb.Router do
      pipe_through :api
 
      resources "/posts/:topic", PostController, except: [:new, :edit]
+     
+     post "/new_user", TokenController, :new
      post "/token", TokenController, :create
      delete "/token", TokenController, :delete
    end
